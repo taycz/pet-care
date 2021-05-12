378689f4ef1dd984a782260e7a3f30d89d196f75
tay crazy <datdvlk62@gmail.com>
2021-05-12 16:48:39 +0700

    update l1

--- template/font-end/page/booking/v_page_booking.php
+++ template/font-end/page/booking/v_page_booking.php
@@ -1,48 +1,115 @@
 <!DOCTYPE html>
 <html lang="en">
-    <?php include ("views/head.php"); ?>
+<?php include ("views/head.php"); ?>
+
 <body>
-<?php include ("views/header.php"); ?>	
+	<?php include ("views/header.php"); ?>
 	<!-- content Booking -->
-	
-    <form action="" method="">
+
+	<form action="" method="post">
 		<div class="contact">
 			<div class="container">
 				<h3 class="agileits-title">Booking Now</h3>
 				<div class="contact-agileinfo">
 					<div class="col-md-7 contact-right">
 						<form action="#" method="post">
-							<input type="text" name="Name" placeholder="Name" required="">
-							<input type="text" class="email" name="Email" placeholder="Email" required="">
-							<input type="text" name="Phone no" placeholder="Phone" required="">
-							<textarea name="Message" placeholder="Message" required=""></textarea>
-							<input type="submit" value="SUBMIT">
+							<input type="text" class="booking-select" name="Name" placeholder="Tên Khách Hàng"
+								required="" data-role="tagsinput">
+							<input type="text" class="booking-select" name="SDT" placeholder="SDT" required="">
+							<div class="choose-time">
+								<div class="choose-time hour">
+									<select class="cTime" id="show-hour" aria-label=".form-select-lg example">
+										<!-- <option value="1">1</option>
+										<option value="2">2</option>
+										<option value="2">2</option> -->
+									</select>
+									<p>Hour</p>
+								</div>
+								<div class="choose-time minutest">
+									<select class="cTime" id="show-minute" aria-label=".form-select-lg example">
+
+										<!-- <option value="1">1</option>
+										<option value="2">2</option>
+										<option value="2">2</option> -->
+									</select>
+									<p>Minute</p>
+								</div>
+							</div>
+							<input type="date" class="booking-date" name="Name" placeholder="Chọn Ngày" required="">
+							<select class="form-select booking-select form-select-lg mb-3"
+								aria-label=".form-select-lg example">
+								<option selected>Chọn Pet</option>
+								<option value="1">Chó</option>
+								<option value="2">Mèo</option>
+
+							</select>
+							<span>Chọn Dịch Vụ</span></br>
+							<select class="choose-services form-select form-select-lg mb-3" multiple="tokenSeparators">
+									<option selected="selected">tỉa lông</option>
+									<option>tắm rửa</option>
+									<option selected="selected">dắt pet đi bộ</option>
+								</select>
+							
+						
+								<select class="form-select booking-select form-select-lg mb-3"
+								aria-label=".form-select-lg example">
+								<option selected>Chọn Nhân Viên</option>
+								<option value="1">Chó</option>
+								<option value="2">Mèo</option>
+
+							</select>
+						
+
+							<input type="submit" class="submit"value="SUBMIT">
 						</form>
 					</div>
-					<div class="col-md-5 contact-left">
-						<div class="address">
-							<h5>Address:</h5>
-							<p><i class="glyphicon glyphicon-home"></i> 333 Broome St New York, NY 10002, </p>
-						</div>
-						<div class="address address-mdl">
-							<h5>Phones:</h5>
-							<p><i class="glyphicon glyphicon-earphone"></i> +1 999 888 777</p>
-							<p><i class="glyphicon glyphicon-phone"></i> +11 222 333</p>
-						</div>
-						<div class="address">
-							<h5>Email:</h5>
-							<p><i class="glyphicon glyphicon-envelope"></i> <a href="mailto:info@example.com">mail@example.com</a></p>
-						</div>
-					</div>
-					<div class="clearfix"></div>
+
+					<!-- <div class="clearfix"></div> -->
 				</div>
 			</div>
 		</div>
 	</form>
-	?>
-<?php
+
+	<?php
 	include ("views/footer.php");
 ?>
+	<script>
+		// seclet Tags
+		$(".choose-services").select2({
+			tags: true,
+			tokenSeparators: [',', ' ']
+		});
+		// for--------->>>>>>>>>hour
+		let showHour=()=>{
+		var hour=19;
+	
+		
+		for(var i= 9;i<= hour;i++){
+			
+			var showHour = showHour+`<option value="1">`+i+`</option>`;
+		}
+		$("#show-hour").empty().append(showHour);
+		}
+		showHour();
+			// for--------->>>>>>>>>minutest
+		let showminute=()=>{
+		var minute = 55;
+		var numMinu=5
+		for(var i= 0;i <= minute;i= i+ 5){
+			// console.log(i);
+			// var showminute = showminute+`<option value="1">`+i+`</option>`;
+			if(i<10){
+				var showminute = showminute+`<option value="1">`+`0`+i+`</option>`;
+			}
+			else{
+				var showminute = showminute+`<option value="1">`+i+`</option>`;
+			}
+
+		}
+		$("#show-minute").empty().append(showminute);
+		}
+		showminute();	
+	</script>
 </body>
 
 </html>
--- views/head.php
+++ views/head.php
@@ -13,22 +13,32 @@
 			window.scrollTo(0, 1);
 		}
 	</script> -->
+	
+	<link href="public/css/style.css" type="text/css" rel="stylesheet" media="all">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
 	<!-- Custom Theme files -->
 	<link href="public/css/bootstrap.css" type="text/css" rel="stylesheet" media="all">
 	<link href="public/css/easy-responsive-tabs.css" rel='stylesheet' type='text/css' />
 	<!-- easy-responsive-tabs -->
-	<!-- <link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all"> -->
-	<link href="public/css/style.css" type="text/css" rel="stylesheet" media="all">
+
 	<link href="public/css/font-awesome.css" rel="stylesheet">
 	<!-- font-awesome icons -->
 	<!-- //Custom Theme files -->
+	
 	<!-- js -->
 	<script src="public/js/jquery-2.2.3.min.js"></script>
 	<!-- //js -->
+	<!-- select2 CDN -->
+	<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 	<!-- web-fonts -->
 	<link href="//fonts.googleapis.com/css?family=Limelight" rel="stylesheet">
 	<link href="//fonts.googleapis.com/css?family=Junge" rel="stylesheet">
 	<link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic'
 	    rel='stylesheet' type='text/css'>
 	<!-- //web-fonts -->
+	<!-- cdn bootrap css -->
+		<!-- <link href="css/bootstrap.css" type="text/css" rel="stylesheet" media="all"> -->
+	
+	
 </head>
